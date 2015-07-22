0e721b96cb56e015920af9c35b18bbeaa53cfc6d
giuseppe <buppa93@gmail.com>
2015-07-22 18:05:55 +0200

    Risolto problema dei checkbox entrambi selezionabili allo stesso momento

--- Test/src/controller/FXMLAlertRegisterController.java
+++ Test/src/controller/FXMLAlertRegisterController.java
@@ -1,16 +1,21 @@
 package controller;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import view.SalesManView;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-public class FXMLAlertRegisterController 
+public class FXMLAlertRegisterController  implements Initializable
 {
 	@FXML CheckBox registered_chbox;
 	@FXML CheckBox notregistered_chbox;
@@ -33,5 +38,53 @@ public class FXMLAlertRegisterController
 	@FXML protected void onCancelEvent(ActionEvent event) throws IOException
 	{
 		((BorderPane) rootPane.getParent()).setCenter(FXMLLoader.load(SalesManView.class.getResource("NothingView.fxml")));
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) 
+	{
+		registered_chbox.selectedProperty().addListener(new ChangeListener<Boolean>()
+				{
+
+					@Override
+					public void changed(
+							ObservableValue<? extends Boolean> observable,
+							Boolean oldValue, Boolean newValue) 
+					{
+						if(newValue == true)
+						{
+							notregistered_chbox.setDisable(true);
+						}
+						
+						if(newValue == false)
+						{
+							notregistered_chbox.setDisable(false);
+						}
+						
+					}
+					
+				});
+		
+		notregistered_chbox.selectedProperty().addListener(new ChangeListener<Boolean>()
+				{
+
+					@Override
+					public void changed(
+							ObservableValue<? extends Boolean> observable,
+							Boolean oldValue, Boolean newValue) 
+					{
+						if(newValue == true)
+						{
+							registered_chbox.setDisable(true);
+						}
+						
+						if(newValue == false)
+						{
+							registered_chbox.setDisable(false);
+						}
+						
+					}
+					
+				});
 	}
 }
